7de44ec344428393683507c59baa7faaef466998
surendra <surendranathbezawada@gmail.com>
2024-10-28 20:31:24 +0530

    myweb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EducationdeatilsComponent } from './educationdeatils/educationdeatils.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'contacts',component:ContactsComponent},
+  {path:'educationdeatils',component:EducationdeatilsComponent},
+  {path:'projects',component:ProjectsComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EducationdeatilsComponent } from './educationdeatils/educationdeatils.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EducationdeatilsComponent,
+    ProjectsComponent,
+    ProfileComponent,
+    LoginComponent,
+    SignupComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
